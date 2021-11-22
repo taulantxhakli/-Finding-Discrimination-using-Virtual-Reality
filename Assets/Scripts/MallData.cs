2566707afbd7671956aea807da675c6764a872f2
tålant <58316986+taulantxhakli@users.noreply.github.com>
2021-11-22 13:40:37 -0500

    scripts

---
+++ Assets/Scripts/MallData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MallData : MonoBehaviour
+{
+    public static float xPos, yPos, zPos;
+    public static int previousSceneLocation = -1;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+}
